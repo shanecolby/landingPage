675b97c9bffe570ee0a152cfb9ba8aacf9784524
shanecolby <shanecolbyzero8@gmail.com>
2022-01-06 17:51:36 -0600

    style and use flexbox to make page

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   .then(res => res.json())
   .then(data => {
     document.body.style.backgroundImage = `url(${data.urls.full})`
-    console.log(data)
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+
   })
