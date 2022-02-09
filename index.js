d9d261ead82189affd7cf80b27fc4197547ed38f
shanecolby <shanecolbyzero8@gmail.com>
2022-02-09 17:20:29 -0600

    style weather and add Ethereum currency to page

--- index.js
+++ index.js
@@ -35,6 +35,31 @@ function getCurrentTime() {
   document.getElementById("time").innerHTML = `${date.toLocaleTimeString("en-us", { timeStyle: "medium" })}`
 }
 
+fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+  .then(res => {
+    if (!res.ok) {
+      throw Error("Something went wrong")
+    }
+    return res.json()
+  })
+  .then(data => {
+    document.getElementById("crypto-bottom").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+    document.getElementById("crypto").innerHTML += `
+    <p>🎯current: $${data.market_data.current_price.usd}</p>
+    <p>👆24hr high: $${data.market_data.high_24h.usd}</p>
+    <p>👇24hr low: $${data.market_data.low_24h.usd}</p>
+`
+  })
+  .catch(err => console.error(err))
+
+function getCurrentTime() {
+  const date = new Date()
+  document.getElementById("time").innerHTML = `${date.toLocaleTimeString("en-us", { timeStyle: "medium" })}`
+}
+
 
 setInterval(getCurrentTime, 1000)
 
@@ -51,9 +76,8 @@ navigator.geolocation.getCurrentPosition(position => {
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
       document.getElementById("weather").innerHTML = `
         <img src=${iconUrl} />
-        <p>${Math.round(data.main.temp)}</p>
-        <p>${data.name}</p>
-        
+        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+        <p class="weather-city">${data.name}</p>
       `
     })
     .catch(err => console.log(err))
