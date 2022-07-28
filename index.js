87055761611dce42bdc56ab5743254c0fdb9bd3a
shanecolby <shanecolbyzero8@gmail.com>
2022-07-28 10:26:41 -0500

    change api image from regular to full

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(res => res.json())
   .then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
   })
   .catch(err => {
